b21ad23b4158c2478c17ded5ec29c6a37640220f
Your Nameriyazulislam99 <riyazulislam0099@gmail.com>
2024-05-16 23:18:07 +0600

    Create Custom Hook Usemenu To Load Menu Data

--- src/pages/Home/PopulerMenu/PopulerMenu.jsx
+++ src/pages/Home/PopulerMenu/PopulerMenu.jsx
@@ -1,24 +1,27 @@
-import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../hooks/useMenu";
 
 const PopulerMenu = () => {
-  const [menu, setMenu] = useState([]);
+  const [menu] = useMenu();
+  const popular = menu.filter((item) => item.category === "popular");
 
-  useEffect(() => {
-    fetch("menu.json")
-      .then((res) => res.json())
-      .then((data) => {
-        const popularItems = data.filter((item) => item.category === "popular");
-        setMenu(popularItems);
-      });
-  }, []);
+  // const [menu, setMenu] = useState([]);
+
+  // useEffect(() => {
+  //   fetch("menu.json")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       const popularItems = data.filter((item) => item.category === "popular");
+  //       setMenu(popularItems);
+  //     });
+  // }, []);
 
   return (
     <section className="max-w-7xl mx-auto">
       <SectionTitle heading="FROM OUR MENU" subHeading={"---Check it out---"} />
       <div className="grid md:grid-cols-2 gap-10">
-        {menu.map((item) => (
+        {popular.map((item) => (
           <MenuItem key={item._id} item={item} />
         ))}
       </div>
--- src/pages/Shared/Cover/Cover.jsx
+++ src/pages/Shared/Cover/Cover.jsx
@@ -1,4 +1,4 @@
-import { Parallax, Background } from "react-parallax";
+import { Parallax } from "react-parallax";
 
 const Cover = ({ img, title }) => {
   return (
